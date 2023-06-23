722e2d946d802cbf180c575b37b2ad51e28112b8
Ukra1n1anUnd3rd0g <impersonalvlad23@gmail.com>
2023-06-23 23:28:38 +0300

    Implemented login testing

--- profiles/login.py
+++ profiles/login.py
@@ -13,20 +13,33 @@ def test(username, email, password):
         "email": email,
         "password": password,
     }
-    result = client.action(schema, action, params=params)
 
-    # JSON response output
-    result = dict(result)
-    result['user'] = dict(result['user'])
-    print(f"JSON response:\n{result}")
-    input()
+    try:
+        # Perform the API request
+        result = client.action(schema, action, params=params)
+
+        # JSON response output
+        result = dict(result)
+        result['user'] = dict(result['user'])
+        print(f"Login is successful. JSON response:\n{result}")
+    except coreapi.exceptions.ErrorMessage as e:
+        print(f"Login failed. Error message: {str(e)}")
 
 
 if __name__ == "__main__":
-    # Enter the input data
-    usrname = input("Name: ")
-    mail = input("Email: ")
-    passwrd = input("Password: ")
+    test_data = [
+        {"username": "john1", "email": "mail2@example.com", "password": "password12345678"},
+        {"username": "john", "email": "mail2@example.com", "password": "password12345678"},
+        {"username": "", "email": "mail2@example.com", "password": "password12345678"},
+        {"username": "john1", "email": "mail2@example.co", "password": "password12345678"},
+        {"username": "john1", "email": "", "password": "password12345678"},
+        {"username": "john1", "email": "mail2@example.com", "password": "password1234"},
+        {"username": "john1", "email": "mail2@example.com", "password": ""},
+    ]
 
-    # Call the test function
-    test(usrname, mail, passwrd)
+    print("\n <========================== Login Testing ===========================>\n\n")
+    for data in test_data:
+        print("Testing with data:", data)
+        test(str(data["username"]), str(data["email"]), str(data["password"]))
+        print()
+    input()
